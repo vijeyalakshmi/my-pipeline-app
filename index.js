7535ca013bb509298e94083d80b390f702d95761
vijeyalakshmi <shwaguru@yahoo.com>
2025-05-08 05:13:21 +0200

    Ensure ESLint is installed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Pipeline! Welcome');
+  res.send('Hello, Pipeline!');
 });
 
 module.exports = app;

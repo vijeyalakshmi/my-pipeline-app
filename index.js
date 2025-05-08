997749d5ed6fb6b4f4b9646723fc413b2ca91d2b
vijeyalakshmi <shwaguru@yahoo.com>
2025-05-08 17:35:32 +0200

    Automatic Deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Pipeline! Welcome Krishna :)');
+  res.send('Hello, Pipeline!');
 });
 
 module.exports = app;

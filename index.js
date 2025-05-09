088fbda73bdfc8e52e7c371b33aecbc3a83b1272
vijeyalakshmi <shwaguru@yahoo.com>
2025-05-09 23:01:30 +0200

    Trial Deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Pipeline! Radhe Krishna');
+  res.send('Hello, Pipeline! Hare Radhe Krishna');
 });
 
 module.exports = app;
